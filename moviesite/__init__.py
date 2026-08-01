457a8297d77f060082c9908ee3e8dbb9f6036bfd
Chernet <chernetalene17@gmail.com>
2026-08-01 21:00:44 +0300

    tiny change

--- moviesite/__init__.py
+++ moviesite/__init__.py
@@ -9,7 +9,10 @@ from flask_mail import Mail
 app=Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL","sqlite:///site.db")
+database_url = os.environ.get("DATABASE_URL")
+if database_url and database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://","postgresql://",1)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_url or "sqlite:///site.db"
 db=SQLAlchemy(app)
 bcrypt=Bcrypt(app)
 login_manager=LoginManager(app)
