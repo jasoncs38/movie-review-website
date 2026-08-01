1acba1683903d8f1626006ef98229f23e5805202
Chernet <chernetalene17@gmail.com>
2026-08-01 20:02:34 +0300

    prepare for deployment

--- moviesite/__init__.py
+++ moviesite/__init__.py
@@ -8,8 +8,8 @@ from flask_mail import Mail
 
 app=Flask(__name__)
 
-app.config['SECRET_KEY']='3d032526142a1a8af7cfbfa062cc8447'
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL","sqlite:///site.db")
 db=SQLAlchemy(app)
 bcrypt=Bcrypt(app)
 login_manager=LoginManager(app)
